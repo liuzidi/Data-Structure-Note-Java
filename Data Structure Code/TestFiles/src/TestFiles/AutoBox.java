684c2fba8eededcdf1ed1a3a0d1776c1ccb3737a
liuzidi <liuzidi17@163.com>
2021-04-15 22:42:30 +0800

    commit

---
+++ Data Structure Code/TestFiles/src/TestFiles/AutoBox.java
@@ -0,0 +1,12 @@
+package TestFiles;
+
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class AutoBox {
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println(Character.isDigit(a));
+    }
+}
