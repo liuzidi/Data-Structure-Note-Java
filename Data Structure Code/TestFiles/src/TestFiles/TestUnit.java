b1dc2b274bf9423ade5cec4e2e905df5ec83d4a2
liuzidi <liuzidi17@163.com>
2021-04-18 20:57:11 +0800

    commit

--- Data Structure Code/TestFiles/src/TestFiles/TestUnit.java
+++ Data Structure Code/TestFiles/src/TestFiles/TestUnit.java
@@ -19,39 +19,26 @@ public class TestUnit {
 }
  class test{
      public static void main(String[] args) {
-         ListNode head =new ListNode();
          ListNode h1,h2,h3,h4,h5;
          h5 =new ListNode(5,null);
          h4 =new ListNode(4,h5);
          h3 =new ListNode(3,h4);
          h2 =new ListNode(2,h3);
          h1 =new ListNode(1,h2);
-         head.next = h1;
 
-         ListNode currentIndex = removeNthFromEnd(head,1);
-         while(currentIndex.next != null){
-             currentIndex = currentIndex.next;
+         ListNode currentIndex = reverseList(h1);
+         while(currentIndex != null){
              System.out.println(currentIndex.val);
+             currentIndex = currentIndex.next;
          }
      }
-     public static ListNode removeNthFromEnd(ListNode head, int n) {
-         ListNode currentIndex = head;
-         int total = 0;//计算有几个节点
-         while(currentIndex.next != null){
-             currentIndex = currentIndex.next;
-             total++;
-         }
-         System.out.println("total ="+ total);
-         currentIndex = head;
-         for(int i = 0; i < total - n; i++){
-             currentIndex = currentIndex.next;
-         }//目前指针指向目标的上一个
-         if(currentIndex.next.next != null){
-             currentIndex.next = currentIndex.next.next;
+     public static ListNode reverseList(ListNode head) {
+         if (head == null || head.next == null){
              return head;
          }
-         currentIndex.next = null;
-         return head;
+         ListNode reverse = reverseList(head.next);
+         head.next.next = head;
+         head.next = null;
+         return reverse;
      }
-
 }
