b58a9a0a77a654438cca09ddf49179b367eb36c6
liuzidi <liuzidi17@163.com>
2021-04-18 10:25:23 +0800

    commit

---
+++ Data Structure Code/TestFiles/src/TestFiles/StringBufferTest.java
@@ -0,0 +1,12 @@
+package TestFiles;
+
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class StringBufferTest {
+    public static void main(String[] args) {
+        StringBuffer res = new StringBuffer();
+        System.out.println(res.toString().equals(""));
+    }
+}
--- Data Structure Code/TestFiles/src/TestFiles/TestUnit.java
+++ Data Structure Code/TestFiles/src/TestFiles/TestUnit.java
@@ -2,32 +2,56 @@ package TestFiles;
 
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * @author:liuzidi
  * @Description:
  */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 public class TestUnit {
-    public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
-            }
-        }
-        return i + 1;
-    }
 }
  class test{
      public static void main(String[] args) {
-         TestUnit t =new TestUnit();
-         int [] a ={0};
-         int length =t.removeDuplicates(a);
-         for (int i = 0; i <length ; i++) {
-             System.out.println(a[i]);
-         }
+         ListNode head =new ListNode();
+         ListNode h1,h2,h3,h4,h5;
+         h5 =new ListNode(5,null);
+         h4 =new ListNode(4,h5);
+         h3 =new ListNode(3,h4);
+         h2 =new ListNode(2,h3);
+         h1 =new ListNode(1,h2);
+         head.next = h1;
 
+         ListNode currentIndex = removeNthFromEnd(head,1);
+         while(currentIndex.next != null){
+             currentIndex = currentIndex.next;
+             System.out.println(currentIndex.val);
+         }
+     }
+     public static ListNode removeNthFromEnd(ListNode head, int n) {
+         ListNode currentIndex = head;
+         int total = 0;//计算有几个节点
+         while(currentIndex.next != null){
+             currentIndex = currentIndex.next;
+             total++;
+         }
+         System.out.println("total ="+ total);
+         currentIndex = head;
+         for(int i = 0; i < total - n; i++){
+             currentIndex = currentIndex.next;
+         }//目前指针指向目标的上一个
+         if(currentIndex.next.next != null){
+             currentIndex.next = currentIndex.next.next;
+             return head;
+         }
+         currentIndex.next = null;
+         return head;
      }
 
 }
