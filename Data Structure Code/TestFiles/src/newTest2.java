cb84bba65ac8a6e267d4322a084c206555b7c14f
liuzidi <liuzidi17@163.com>
2021-04-27 20:22:03 +0800

    commit

---
+++ Data Structure Code/TestFiles/src/newTest2.java
@@ -0,0 +1,17 @@
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class newTest2 {
+    public static void main(String[] args) {
+       for(Integer data : test()){
+           System.out.println(data);
+       }
+
+
+
+    }
+    public static Integer[] test(){
+        return new Integer[]{1};
+    }
+}
