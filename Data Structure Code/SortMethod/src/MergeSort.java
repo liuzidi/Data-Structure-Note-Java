ad3aa345e3b5239493d6c5d69652d39d11c522e7
liuzidi <liuzidi17@163.com>
2021-04-29 20:07:08 +0800

    commit

--- Data Structure Code/SortMethod/src/MergeSort.java
+++ Data Structure Code/SortMethod/src/MergeSort.java
@@ -1,28 +1,47 @@
 /**
  *
- *
+ *需要申请一个相同长度的数组来存储有序的数列
  *
  */
 public class MergeSort {
     public static void main(String[] args){
+        int[] nums = {6,75,45,25,4,1,5,82,15,82,5,3,41,2};
+        int[]res = mergeSort(nums);
+        for(int num : res){
+            System.out.println(num);
+        }
 
     }
     public static int[] mergeSort(int[] nums){
-        //分
         int len = nums.length;
-        int middle = len / 2;
-        int left = 0;
-        int right = len - 1;
-
-        return merge(nums,left,right);
+        int[] res = new int[len];
+        mergeSortRecursion(nums,res,0,len - 1);
+        return res;
     }
 
-    private static int[] merge(int[] nums, int left, int right){
-        if(left < right){
-
+    private static void mergeSortRecursion(int[] nums, int[] res, int left, int right){
+        if(left >= right){
+            return;
         }
-
-
-        return null;
+        int middle = left + ((right - left) >> 1);
+        int left1 = left;
+        int right1 = middle;
+        int left2 = middle + 1;
+        int right2 = right;
+        mergeSortRecursion(nums,res,left1,right1);
+        mergeSortRecursion(nums,res,left2,right2);
+        int currentIndex = left;//从给到的数组的最左端开始填入
+        while(left1 <= right1 && left2 <= right2){
+            res[currentIndex++] = nums[left1] < nums[left2] ? nums[left1++] : nums[left2++];
+        }
+        while(left1 <= right1){//右部分已经排好序或者是空
+            res[currentIndex++] = nums[left1++];
+        }
+        while(left2 <= right2){
+            res[currentIndex++] = nums[left2++];
+        }
+        for(int i = left; i <= right; i++){
+            nums[i] = res[i];
+        }
     }
 }
