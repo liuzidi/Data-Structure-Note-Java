2b1951dd360a0c66e9db408f48738b1848b3c63a
liuzidi <liuzidi17@163.com>
2021-05-15 15:45:44 +0800

    commit

---
+++ Data Structure Code/TestFiles/src/TestFiles/Test514.java
@@ -0,0 +1,14 @@
+package TestFiles;
+
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class Test514 {
+    public static void main(String[] args) {
+        String a = "0123";
+        double b = (double) 1 / 6;
+        System.out.println(b);
+
+    }
+}
