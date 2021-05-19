06604b67186a6b772ff9eb5db0e026d8e09218f1
liuzidi <liuzidi17@163.com>
2021-05-19 15:16:38 +0800

    commit

---
+++ Data Structure Code/TestFiles/src/TestFiles/Test0519.java
@@ -0,0 +1,15 @@
+package TestFiles;
+
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class Test0519 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][];
+
+        System.out.println(matrix[2].length);
+
+    }
+
+}
