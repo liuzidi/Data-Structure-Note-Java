5f59d8073ed32bbc28f09e3644d0311ef3e21cb7
liuzidi <liuzidi17@163.com>
2021-05-20 20:26:19 +0800

    commit

--- Data Structure Code/TestFiles/src/TestFiles/AutoBox.java
+++ Data Structure Code/TestFiles/src/TestFiles/AutoBox.java
@@ -1,12 +1,36 @@
 package TestFiles;
 
+import java.util.*;
+
 /**
  * @author:liuzidi
  * @Description:
  */
 public class AutoBox {
     public static void main(String[] args) {
-        char a = 'a';
-        System.out.println(Character.isDigit(a));
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        for(Integer i : list){
+            System.out.println(i);
+        }
+        System.out.println(add(3, 5));
+        Integer i = 7;
+        System.out.println(compare(1, 4));
+        System.out.println(compare(i, 4));
+        System.out.println(compare(7, 7));
+    }
+    public static Integer add(Integer a, Integer b){
+        int aInt = a;
+        int bInt = b;
+        return (a+b);
+    }
+    public static boolean compare(Integer a, Integer b){
+        if(a < b){
+            return true;
+        }else if(a.equals(b)){
+            System.out.println("可以发生相等事件");
+        }
+        return false;
     }
 }
