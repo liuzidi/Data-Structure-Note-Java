a6f712ecd00187da88d73d618a39ccfd7229000c
liuzidi <liuzidi17@163.com>
2021-07-19 21:49:29 +0800

    LeetCode刷题笔记

--- Data Structure Code/src/LeetCode/topHot100_416.java
+++ Data Structure Code/src/LeetCode/topHot100_416.java
@@ -1,30 +1,36 @@
 package LeetCode;
 
-import java.util.Arrays;
-
 /**
  * @author:liuzidi
  * @Description:
  */
 public class topHot100_416 {
-    public boolean canPartition(int[] nums) {
-        if(nums.length < 2) return false;
-        int addPlus = 0;
-        for(int num : nums){
-            addPlus += num;
-        }
-        if(addPlus % 2 != 0) return false;
-        int target = addPlus / 2;
-        boolean[] dp = new boolean[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            dp[i] = DFS(nums,target,i);
-        }
-        return null;
-    }
-    //判断nums的前len个元素能不能够凑成target和,即索引0到index的元素
-    public boolean DFS(int[] nums, int target, int index){
-        if(index == 0){
-            return nums[0] == target;
+    class Solution {
+        public boolean canPartition(int[] nums) {
+            if(nums.length < 2) return false;
+            int plus = 0;
+            for(int num: nums){
+                plus += num;
+            }
+            if(plus % 2 != 0) return false;
+            int target = plus / 2;
+            boolean[][] dp = new boolean[nums.length][target + 1];
+            for(int i = 0; i < target + 1; i++){
+                dp[0][i] = nums[0] == target;
+            }
+            for(int i = 1; i < nums.length; i++){
+                for(int j = 0; j < target + 1; j++){
+                    if(dp[i - 1][j] || nums[i] == j){
+                        dp[i][j] = true;
+                    }else{
+                        if(j > nums[i])
+                            dp[i][j] = dp[i - 1][j - nums[i]];
+                        else
+                            dp[i][j] = false;
+                    }
+                }
+            }
+            return dp[nums.length - 1][target];
         }
     }
 }
