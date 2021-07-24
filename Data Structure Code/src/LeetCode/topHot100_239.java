8937c7c90db4213cf0619508d7b3624a0e64a728
liuzidi <liuzidi17@163.com>
2021-07-24 11:55:08 +0800

    LeetCode刷题笔记

---
+++ Data Structure Code/src/LeetCode/topHot100_239.java
@@ -0,0 +1,49 @@
+package LeetCode;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class topHot100_239 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        //默认小顶堆
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, (o1, o2)->o2 - o1);
+        for(int i = 0; i < k; i++){
+            pq.add(nums[i]);
+        }
+        int[] res = new int[nums.length - k];
+        if(!pq.isEmpty())
+        res[0] = pq.peek();
+        for(int i = k; i < nums.length; i++){
+            pq.remove(nums[i - k]);
+            pq.add(nums[i]);
+            res[i - k + 1] = pq.peek();
+        }
+        return res;
+    }
+
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length; i++) {
+            if (queue.isEmpty()) {
+                queue.addLast(i);
+            }else{
+                if(queue.size() == k || i - k == queue.peekFirst()){
+                    queue.removeFirst();
+                }
+                while(!queue.isEmpty() && nums[i] > nums[queue.peekLast()]){
+                    queue.removeLast();
+                }
+                queue.addLast(i);
+            }
+            if(i >= k - 1){
+                res[i - k + 1] = nums[queue.peekFirst()];
+            }
+        }
+        return res;
+    }
+}
