bc95bc90282e217a3492259fe9c1959cff571866
liuzidi <liuzidi17@163.com>
2021-09-05 14:47:30 +0800

    LeetCode刷题笔记

---
+++ Data Structure Code/src/LeetCode/classicProblem/P03.java
@@ -0,0 +1,22 @@
+package LeetCode.classicProblem;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author:liuzidi
+ * @Description:
+ */
+public class P03 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x1,x2)->(x2 - x1));
+        for(int num : nums){
+            pq.offer(num);
+        }
+        int res = 0;
+        for(int i = 0; i < k; i++){
+            res = pq.poll();
+        }
+        return res;
+
+    }
+}
