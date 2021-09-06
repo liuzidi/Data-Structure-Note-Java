d8b3d1e418da775063aaa2f8f0bac0216fabcde3
liuzidi <liuzidi17@163.com>
2021-09-06 20:29:55 +0800

    LeetCode刷题笔记

--- Data Structure Code/src/SortMethod/MergeSort.java
+++ Data Structure Code/src/SortMethod/MergeSort.java
@@ -1,54 +1,42 @@
 package SortMethod;
 
 /**
- *
- *需要申请一个相同长度的数组来存储有序的数列
- * 归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
- *
- * 算法描述:
- * 1把长度为n的输入序列分成两个长度为n/2的子序列；
- * 2对这两个子序列分别采用归并排序；
- * 3将两个排序好的子序列合并成一个最终的排序序列。
+ *递归写法
  *
  */
 public class MergeSort {
     public static void main(String[] args){
         int[] nums = {6,75,45,25,4,1,5,82,15,82,5,3,41,2};
-        int[] res = mergeSort(nums);
-        for(int num : res){
+        sort(nums);
+        for(int num : nums){
             System.out.println(num);
         }
     }
 
-    public static int[] mergeSort(int[] nums){
-        int len = nums.length;
-        int[] res = new int[len];
-        mergeSortRecursion(nums,res,0,len - 1);
-        return res;
+    public static void sort(int[] nums){
+        mSort(nums,0,nums.length - 1);
     }
-
-    private static void mergeSortRecursion(int[] nums, int[] res, int left, int right){
-        if(left >= right){
-            return;
+    public static void mSort(int[] nums, int left, int right){
+        if(left >= right) return;
+        int mid = left + (right - left) / 2;
+        mSort(nums, left, mid);
+        mSort(nums,mid + 1, right);
+        int[] newNums = new int[right -left + 1];
+        int l1 = left, l2 = mid + 1, i = 0;
+        while(l1 <= mid && l2 <= right){
+            if(nums[l1] < nums[l2]){
+                newNums[i++] = nums[l1++];
+            }else{
+                newNums[i++] = nums[l2++];
+            }
         }
-        int middle = left + ((right - left) >> 1);
-        int left1 = left;
-        int left2 = middle + 1;
-        mergeSortRecursion(nums,res,left1, middle);
-        mergeSortRecursion(nums,res,left2, right);
-        int currentIndex = left;//从给到的数组的最左端开始填入
-        while(left1 <= middle && left2 <= right){
-            res[currentIndex++] = nums[left1] < nums[left2] ? nums[left1++] : nums[left2++];
+        while(l1 <= mid){
+            newNums[i++] = nums[l1++];
         }
-        while(left1 <= middle){//右部分已经排好序或者是空
-            res[currentIndex++] = nums[left1++];
+        while(l2 <= right){
+            newNums[i++] = nums[l2++];
         }
-        while(left2 <= right){
-            res[currentIndex++] = nums[left2++];
-        }
-        while(left < right){
-            nums[left++] = res[left++];
-        }
+        System.arraycopy(newNums, 0, nums, left, right + 1 - left);
     }
 
 }
--- Data Structure Code/src/SortMethod/MergeSort2.java
+++ Data Structure Code/src/SortMethod/MergeSort2.java
@@ -2,30 +2,21 @@ package SortMethod;
 
 /**
  * @author:liuzidi
- * @Description:
+ * @Description:迭代写法
  */
 public class MergeSort2 {
     public static void main(String[] args) {
-        int[] nums = {56,2,45,25};
+        int[] nums = {56,2,45,25,3,5,7,8,4,0,46,86};
         sort(nums);
-    }
-    public static void sort(int[] nums){
-        mergeSort(nums,0, nums.length - 1 );
         for(int num : nums){
             System.out.println(num);
         }
     }
-    private static void mergeSort(int[] nums, int l, int r){
+    public static void sort(int[] nums){
+        mergeSort(nums,0, nums.length - 1 );
+    }
+    private static void merge(int[] nums, int l, int mid, int r){
         if(l >= r) return;
-        int mid = l + (r - l) / 2;
-        /**
-         * 分解
-         */
-        mergeSort(nums, l, mid);
-        mergeSort(nums, mid + 1, r);
-        /**
-         * l到mid为有序序列, mid+1到r为有序队列, 接下来进行合并
-         */
         int[] res = new int[r - l + 1];
         int lIndex = l, rIndex = mid + 1, resIndex = 0;
         while(lIndex <= mid && rIndex <= r){
@@ -38,6 +29,19 @@ public class MergeSort2 {
         while(lIndex <= mid) res[resIndex++] = nums[lIndex++];
         while(rIndex <= r)   res[resIndex++] = nums[rIndex++];
         System.arraycopy(res, 0, nums, l, r + 1 - l);
-
+    }
+    public static void mergeSort(int[] nums, int l, int r){
+        int step = 1;
+        int len = nums.length;
+        while(step < len){
+            int offset = step * 2;
+            for(int i = 0; i < len; i += offset){
+                int left = i;
+                int mid = Math.min(i + step - 1, len - 1);
+                int right = Math.min(i + offset - 1, len - 1);
+                merge(nums, left, mid, right);
+            }
+            step *= 2;
+        }
     }
 }
